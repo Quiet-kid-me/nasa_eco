f9f6e353b1d3ae8cf7beeca3f05e4b313730ed23
Quiet Kid <nourhanmm3030@gmail.com>
2024-10-06 19:59:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+import pandas as pd
+st.title('🌍 Eco_Equality')
+st.info('This app shows the number of women in reproductive age having anemia.')
+with st.expander('Data'):
+  st.write('Raw data')
+  df = pd.read_csv('https://raw.githubusercontent.com/Quiet-kid-me/women_anemia/refs/heads/main/data.csv')
+  st.dataframe(df)
